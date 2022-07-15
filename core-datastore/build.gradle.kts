f1e0a31e65e37384d19de66c80700f5cf0cce0f6
duychu1 <duychu1@gmail.com>
2022-07-16 06:37:25 +0700

    Add core-datastore module.

---
+++ core-datastore/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id(Plugins.android_library)
+    kotlin(Plugins.kotlin_android)
+}
+
+android {
+    compileSdk = Config.targetSdkVersion
+
+    defaultConfig {
+        minSdk = Config.minSdkVersion
+        targetSdk = Config.targetSdkVersion
+
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.Androidx.datastore_prefer)
+    implementation(Dependencies.coroutines)
+
+}
